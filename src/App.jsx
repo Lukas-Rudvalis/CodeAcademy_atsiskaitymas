f4c44e51a093bfb5e9f87dc7c550961929741154
Lukas Rudvalis <lukas.rudvalis@gmail.com>
2023-04-20 15:11:02 +0300

    global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
-import './styles/reset.css';
-import './styles/variables.css';
+// import './styles/reset.css';
+// import './styles/variables.css';
 import ShopsPage from './pages/ShopsPage';
 import AddShopPage from './pages/AddShopPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<ShopsPage />} />
         <Route path="/add" element={<AddShopPage />} />
