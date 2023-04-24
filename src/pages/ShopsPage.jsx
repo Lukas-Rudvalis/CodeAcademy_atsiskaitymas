26bc8f2db49e893eddf3dc8aa368a103261e51af
Lukas Rudvalis <lukas.rudvalis@gmail.com>
2023-04-24 21:06:36 +0300

    town filter

--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../components/ui/Title';
 import Container from '../components/ui/Container';
 import ShopsList from '../components/shops/ShopsList';
@@ -8,8 +8,11 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import Popup from '../components/ui/Popup';
+import Button from '../components/ui/Button';
 
 function ShopsPage() {
+  const [filter, setFilter] = useState('All');
+
   const ref = collection(db, 'shops');
   const q = query(ref, orderBy('timestamp', 'desc'));
   const [value, loading, error] = useCollection(q);
@@ -19,11 +22,32 @@ function ShopsPage() {
       uid: doc.id,
       ...doc.data(),
     }));
+
+  // get unique towns
+  const uTowns = ['All'];
+  docsWithUid?.forEach((doc) => {
+    if (!uTowns.includes(doc.town)) uTowns.push(doc.town);
+  });
+
+  const filteredDocsWithUid =
+    filter === 'All'
+      ? docsWithUid
+      : docsWithUid?.filter((doc) => doc.town === filter);
+
   return (
     <>
       {error && <Popup type="error">{error.code}</Popup>}
       {loading && <Popup type="info">Loading...</Popup>}
       {value?.size && <Popup type="success">Loaded successfully</Popup>}
+      <Container>
+        <Filters className="mb20">
+          {uTowns.map((town) => (
+            <li key={town}>
+              <Button onClick={() => setFilter(town)}>{town}</Button>
+            </li>
+          ))}
+        </Filters>
+      </Container>
       <ShopsContainer>
         <Title fz={4} className="tac mb20">
           Shops
@@ -33,11 +57,16 @@ function ShopsPage() {
             There are no shops yet. Create one <Link to={'/add'}>here</Link>{' '}
           </Text>
         )}
-        {value && <ShopsList list={docsWithUid} />}
+        {value && <ShopsList list={filteredDocsWithUid} />}
       </ShopsContainer>
     </>
   );
 }
+
+const Filters = styled.ul`
+  display: flex;
+  gap: 10px;
+`;
 
 const ShopsContainer = styled(Container)`
   display: flex;
